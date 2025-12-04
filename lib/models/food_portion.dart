9a3f6685f77ec2518b9534dac40a851593ee13cb
Chad DeRosier <chad.derosier@tutanota.com>
2025-12-04 10:46:58 -0800

    added comments

--- lib/models/food_portion.dart
+++ lib/models/food_portion.dart
@@ -2,8 +2,8 @@ import 'package:free_cal_counter1/models/food.dart';
 
 class FoodPortion {
   final Food food;
-  final double grams;
-  final String unit;
+  final double grams; // weight of the portion in grams
+  final String unit; // unit of measurement of the portion
 
   FoodPortion({required this.food, required this.grams, required this.unit});
 }
